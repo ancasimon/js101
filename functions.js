0818ec95b171291063860d5ecb6aaff83600df01
Anca Simon <ancarsimon@gmail.com>
2020-01-20 18:51:39 -0600

    added dogBreed

--- functions.js
+++ functions.js
@@ -8,3 +8,11 @@ const nuggetizer = (animal) => {
 }
 console.log(nuggetizer('chicken'));
 console.log(nuggetizer('pig'));
+
+const dogBreed = (dog) => {
+    const output = `My favorite dog breed is ${dog}.`;
+    return output;
+};
+
+console.log(dogBreed('lab'));
+console.log(dogBreed('actually a cat'));
